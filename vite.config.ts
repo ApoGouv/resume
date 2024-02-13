21b17d5fa1fd1b941f03c6ddf3233cadbc797f56
ApostolosG <apo.gouv@gmail.com>
2024-02-13 07:51:33 +0200

    Improve build step

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @see https://vitejs.dev/config/shared-options.html#base
+  base: '/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-    pool: 'forks',
-  },
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: ['./src/setupTests.ts'],
+  //   pool: 'forks',
+  // },
 });
