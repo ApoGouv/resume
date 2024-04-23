434c33027301389641fff24b70948a8c39331c48
Apostolos Gouvalas <apo.gouv@gmail.com>
2024-04-23 09:30:16 +0300

    Postcss setup, Local Google Fonts and Assets organizing on build step

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import LocaleProvider from './Context/ThemeContext';
 import ResumePage from './Pages/ResumePage/ResumePage';
+
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/400-italic.css';
+import '@fontsource/lato/700.css';
+import '@fontsource/lato/900.css';
+
 import './index.css';
 import reportWebVitals from './Utils/reportWebVitals';
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, UserConfig } from 'vite';
 
 // @see https://vitejs.dev/config/#conditional-config
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
@@ -14,20 +14,58 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
     isPreview,
   });
 
-  const config = {
+  const config: UserConfig = {
     // @see https://vitejs.dev/config/shared-options.html#base
     base: '/',
     plugins: [react()],
-    // test: {
-    //   globals: true,
-    //   environment: 'jsdom',
-    //   setupFiles: ['./src/setupTests.ts'],
-    //   pool: 'forks',
-    // },
+    build: {
+      minify: false,
+      // sourcemap: true,
+      rollupOptions: {
+        output: {
+          dir: 'dist',
+          // more info https://rollupjs.org/configuration-options/#output-assetfilenames
+          assetFileNames: (assetInfo): string => {
+            // console.log(
+            //   ` 🪛 Vite Config build > rollupOptions > assetFileNames > assetInfo Name:`,
+            //   {
+            //     assetInfoName: assetInfo?.name ?? '',
+            //   }
+            // );
+
+            const assetName: string = assetInfo?.name ?? ''; // Handle undefined case
+
+            if (!assetName) {
+              throw new Error('Unexpected condition, assetInfo had no name');
+            }
+            const assetsFolder = 'assets/';
+            const extType: string =
+              assetName.split('.').at(1)?.toLowerCase() ?? '';
+
+            let assetsSubFolder = '';
+            if (/png|jpe?g|svg|webp|gif|tiff|bmp|ico/i.test(extType)) {
+              assetsSubFolder = 'images/';
+            } else if (/css|sass|scss/i.test(extType)) {
+              assetsSubFolder = 'css/';
+            } else if (/woff|woff2|eot|ttf|otf/i.test(extType)) {
+              assetsSubFolder = 'fonts/';
+            }
+
+            return `${assetsFolder}${assetsSubFolder}[name]-[hash][extname]`;
+          }, // END of assetFileNames
+          chunkFileNames: 'assets/js/[name].[hash].js',
+          entryFileNames: 'assets/js/[name].[hash].js',
+        }, // End of output
+      }, // End of rollupOptions
+    }, // End of build
   };
 
   if (command === 'build' || (command === 'serve' && mode === 'production')) {
     config.base = '/resume';
+
+    if (config.build !== undefined) {
+      config.build.minify = true;
+    }
   }
 
   return config;
