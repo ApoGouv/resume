2522b999cebef60d1749e32599443507748e174c
Apostolos Gouvalas <apo.gouv@gmail.com>
2024-09-30 14:50:11 +0300

    updated custom d

--- custom.d.ts
+++ custom.d.ts
@@ -1,12 +1,8 @@
-declare module '*.png';
 declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
-// declare module "*.svg" {
-//   const content: string;
-//   export default content;
-// }
+declare module '*.png';
 declare module '*.jpeg';
 declare module '*.jpg';
 declare module '*.webp';
