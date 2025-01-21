468e6ce3f50c4d5711ee42670765536300ba244d
Apostolos Gouvalas <apo.gouv@gmail.com>
2025-01-21 15:17:57 +0200

    Version update

--- src/constants.ts
+++ src/constants.ts
@@ -36,7 +36,7 @@ export const DEFAULT_LOCALE = EL_LOCALE;
  * version of the resume.
  * @constant
  */
-export const VERSION_FALLBACK = '3.0.1fx';
+export const VERSION_FALLBACK = '4.0.0fx';
 
 /**
  * Placeholder for string/text that will be replaced by years of experience number.
