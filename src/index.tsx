e014935d51968df8fdcb35502fda1f2c52c11c48
Apostolos Gouvalas <apo.gouv@gmail.com>
2025-01-29 13:35:39 +0200

    removed local resumeAppInDev in favor of similar variable from our constants

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import { HashRouter } from 'react-router-dom';
 import ThemeProvider from '@/Context/ThemeContextProvider';
 import ResumePage from '@/Pages/ResumePage/ResumePage';
 
+import { APP_IN_DEV_MODE } from '@/constants';
+import reportWebVitals from '@/Utils/reportWebVitals';
+
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/400-italic.css';
 import '@fontsource/lato/700.css';
 import '@fontsource/lato/900.css';
 
 import '@/index.css';
-import reportWebVitals from '@/Utils/reportWebVitals';
-
-const resumeAppInDev = import.meta.env?.DEV ?? false;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -27,7 +27,7 @@ root.render(
   </React.StrictMode>
 );
 
-if (resumeAppInDev) {
+if (APP_IN_DEV_MODE) {
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
